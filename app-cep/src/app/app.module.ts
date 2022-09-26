85aa607ac8c4118ecf98ee156c193debee11692a
ThiagoSoares <thiagocsoares@gmail.com>
2022-09-26 06:36:22 -0300

    criado componente proprietario

--- app-cep/src/app/app.module.ts
+++ app-cep/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProprietarioComponent } from './proprietario/proprietario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProprietarioComponent
   ],
   imports: [
     BrowserModule
